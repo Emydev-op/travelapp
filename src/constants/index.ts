d81c179efd3d1cdb9f381d54600c5e0832dc4981
Emydev-op <alphaoseghe@gmail.com>
2024-04-26 18:55:11 +0100

    complete ui

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -62,15 +62,15 @@ export const FOOTER_LINKS = [
   },
   {
     title: "Our Community",
-    links: ["Climbing xixixi", "Hiking hilink", "Hilink kinthill"],
+    links: ["Climbing Community", "Hunting Community", "Ski Community"],
   },
 ];
 
 export const FOOTER_CONTACT_INFO = {
   title: "Contact Us",
   links: [
-    { label: "Admin Officer", value: "123-456-7890" },
-    { label: "Email Officer", value: "hilink@akinthil.com" },
+    { label: "Admin Officer", value: "0815 836 4325" },
+    { label: "Email Officer", value: "alphaoseghe@gmail.com" },
   ],
 };
 
